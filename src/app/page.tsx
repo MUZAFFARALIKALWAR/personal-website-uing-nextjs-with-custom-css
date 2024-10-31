0486b2a95a4be5d1b7d56f04cc8fc05ed65c894e
Muzafar Ali <[valid-email]>
2024-11-01 01:41:36 +0500

    upload personal web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Blog from "@/components/blog";
+
 import Herro from "@/components/herro";
 import Priceplan from "@/components/plan";
 import Portfolio from "@/components/portfolio";
@@ -12,7 +12,7 @@ export default function Home() {
           <Priceplan/>
           <Recommendations/>
           <Portfolio/>
-          <Blog/>
+          
          </div>
   );
 }
